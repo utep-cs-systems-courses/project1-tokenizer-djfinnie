374248d1eee57dda67778a8a217fcf659ca57d9c
djfinnie <djfinnie.miners.utep.edu>
2020-09-16 15:11:22 -0600

    Created Makefile, space and non_space functions, word_start & word_terminator

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -28,6 +28,7 @@ int non_space_char(char c)
 {
   if (c >= '!') return 1;
   return 0;
+}
 
 
 
@@ -37,4 +38,90 @@ int non_space_char(char c)
 
    str does not contain any words. */
 
-char word_start(char *str); 
+char *word_start(char *str)
+{
+  char* p = str;
+  while (space_char(p[0]))
+    {
+      if (non_space_char(p[0])) return p;
+      *p++;
+    }
+  return p;
+}
+
+/* Returns a pointer terminator char following *word */
+
+char *word_terminator(char *word)
+{
+  char *p = word;
+  while (p[0] != 0)
+    {
+      *p++;
+    }
+  return p;
+} 
+
+
+
+/* Counts the number of words in the string argument. */
+
+int count_words(char *str)
+{
+  char *p = str;
+  int num_words = 0;
+  char *term = *word_terminator(&p[0]);
+  while (*p != *term)
+    {
+      if (*word_start(&p[0]) != *term)
+	{
+	  num_words++;
+	  while (non_space_char(p[0]))
+	    {
+	      *p++;
+	    }
+	}
+    }
+  return num_words;
+}
+	  
+
+
+
+ /* Returns a fresly allocated new zero-terminated string 
+
+    containing <len> chars from <inStr> */
+
+char *copy_str(char *inStr, short len);
+
+
+
+ /* Returns a freshly allocated zero-terminated vector of freshly allocated 
+
+   space-separated tokens from zero-terminated str.
+
+   For example, tokenize("hello world string") would result in:
+
+     tokens[0] = "hello"
+
+     tokens[1] = "world"
+
+     tokens[2] = "string" 
+
+     tokens[3] = 0
+
+ */
+
+char **tokenize(char* str);
+
+
+
+ /* Prints all tokens. */
+
+ void print_tokens(char **tokens);
+
+
+
+ /* Frees all tokens and the vector containing themx. */
+
+ void free_tokens(char **tokens);
+
--- src/uimain.c
+++ src/uimain.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 #include "tokenizer.h"
+#include <string.h>
 
 int main()
 {
+  char s[] = "     my dog is flea free";
   puts("Welcome!");
-  printf(space_char(" "));
-  printf(non_space_char("6"));
+
+  printf("\nword_start:");
+  printf("\nStart of string:%s ", s);
+  printf("\nfirst non space char: %c\n", *word_start(&s[0]));
+
+  printf("Word terminator: %c\n", *word_terminator(&s[0]));
+
+  printf("Number of words in %s -", s);
+  printf("%d", count_words(&s[0]));
+
+
   return 0;
 }
